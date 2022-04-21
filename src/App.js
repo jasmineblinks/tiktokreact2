163508985be8e155523039de9747a8b1d7760ad6
jasmineblinks <puenehfaith@gmail.com>
2022-04-21 01:40:16 +0100

    First update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/Header";
+import ProfileHeader from "./components/profileheader/ProfileHeader";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<ProfileHeader />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/profileheader/ProfileHeader.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import tiktoklogo from "../../images/tiktoklogo.png";
+import dots from "../../images/menu-dots.svg";
+import search from "../../images/search.svg";
+import { Link } from "react-router-dom";
+
+function ProfileHeader() {
+  return (
+    <div className="nav-bar">
+      <img src={tiktoklogo} className="app-logos" alt="logo" />
+      <form className="form__banner">
+        <input
+          className="search__input"
+          type="text"
+          placeholder="Search accounts and videos"
+          name="search"
+        />
+        <button className="search__submit" type="submit">
+          <img src={search} alt="logo" />
+        </button>
+      </form>
+      <ul className="nav-links">
+        <li>Upload</li>
+
+        <Link to="/profile" style={{ textDecoration: "none" }}>
+          <li className="btn">Log in</li>
+        </Link>
+        <li>
+          <img src={dots} alt="logo" />
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default ProfileHeader;
