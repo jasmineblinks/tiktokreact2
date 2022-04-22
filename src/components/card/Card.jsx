af5ba260a129b05b8f6ce44a5fa8455bd47c5c73
jasmineblinks <puenehfaith@gmail.com>
2022-04-22 02:16:12 +0100

    fix:updated the card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -19,44 +19,51 @@ function Card() {
   };
   return (
     <div className="card-container">
-      <div className="break" />
-      <div className="card-section">
-        <div className="user-info">
-          {/* <img className="user-profile" src={profile} alt="profile" />s */}
-          <div>
-            <div className="section-profile">
-              <h3 className="bold">emmzy</h3>
-              <p className="username">emma mbonu</p>
+      <div className="center-div">
+        <div className="break" />
+        <div className="card-section">
+          <div className="user-info">
+            {/* <img className="user-profile" src={profile} alt="profile" />s */}
+            <div>
+              <div className="section-profile">
+                <h3 className="bold">emmzy</h3>
+                <p className="username">emma mbonu</p>
+              </div>
+              <h5>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Tenetur.
+              </h5>
             </div>
-            <h5>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur.
-            </h5>
           </div>
         </div>
-      </div>
-      <div className="video-socials">
-        <video className="video" controls ref={videoRef} onClick={onVideoPress}>
-          <source
-            src="https://v16-webapp.tiktok.com/e4acd1d6617991162bdd67a44528d24d/62584776/video/tos/useast2a/tos-useast2a-ve-0068c001/9a3edbe7c4bb4b8081383916fb376214/?a=1988&br=612&bt=306&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=eXd.6HF2Myq8ZJf~hwe2N9twyl7Gb&l=2022041410084101019018602909029870&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anY6dzQ6ZmU4PDMzNzczM0ApOjk4N2U8ZWU0NzxnZWgzO2c1NDVscjQwZC1gLS1kMTZzczJiYV8xNTJeYDYuYzYwL146Yw%3D%3D&vl=&vr="
-            type="video/mp4"
-          />
-        </video>
-        <div className="section-content socials">
-          <div className="icon">
-            <img src={heart} width={"25px"} alt="heart" />
-          </div>
+        <div className="video-socials">
+          <video
+            className="video"
+            controls
+            ref={videoRef}
+            onClick={onVideoPress}>
+            <source
+              src="https://v16-webapp.tiktok.com/e4acd1d6617991162bdd67a44528d24d/62584776/video/tos/useast2a/tos-useast2a-ve-0068c001/9a3edbe7c4bb4b8081383916fb376214/?a=1988&br=612&bt=306&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=eXd.6HF2Myq8ZJf~hwe2N9twyl7Gb&l=2022041410084101019018602909029870&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anY6dzQ6ZmU4PDMzNzczM0ApOjk4N2U8ZWU0NzxnZWgzO2c1NDVscjQwZC1gLS1kMTZzczJiYV8xNTJeYDYuYzYwL146Yw%3D%3D&vl=&vr="
+              type="video/mp4"
+            />
+          </video>
+          <div className="section-content socials">
+            <div className="icon">
+              <img src={heart} width={"25px"} alt="heart" />
+            </div>
 
-          <div className="social-tag">5.1m</div>
-          <div className="icon">
-            <img src={comment} width={"25px"} alt="comment" />
-          </div>
+            <div className="social-tag">5.1m</div>
+            <div className="icon">
+              <img src={comment} width={"25px"} alt="comment" />
+            </div>
 
-          <div className="social-tag">20.3k</div>
-          <div className="icon">
-            <img src={share} width={"25px"} alt="share" />
-          </div>
+            <div className="social-tag">20.3k</div>
+            <div className="icon">
+              <img src={share} width={"25px"} alt="share" />
+            </div>
 
-          <div className="social-tag">4000</div>
+            <div className="social-tag">4000</div>
+          </div>
         </div>
       </div>
     </div>
