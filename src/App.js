35b7b5a706e1b98302c626b27f4ccf982dd6f9df
jasmineblinks <puenehfaith@gmail.com>
2022-04-22 13:51:24 +0100

    feat:added the upload and uploadpage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home/Home";
 import Profile from "./components/profile/Profile";
+import UploadPage from "./components/uploadpage/UploadPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/profile" element={<Profile />} />
+        <Route exact path="/upload" element={<UploadPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/upload/Upload.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./upload.css";
+
+function Upload() {
+  return (
+    <div className="upload-banner">
+      <div className="upload-content">
+        <h3>Uploads</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Upload;
